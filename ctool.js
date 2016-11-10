232e27da1cf6acca214e656278cb6639a596afe3
jiangbai333@163.com <jiangbai333@163.com>
2016-11-10 08:56:40 +0800

    function getTimestamp 获取当前时间戳

--- ctool.js
+++ ctool.js
@@ -6,19 +6,29 @@
 
 /**
  * 判断传入数据类型
- * typeOf({}) => 'object'
- * typeOf([]) => 'array'
- * typeOf('') => 'string'
- * typeOf(.1) => 'number'
- * typeOf(/x/) => 'regexp'
- * typeOf(null) => 'null'
- * typeOf() => 'undefined'
- * typeOf(undefined) => 'undefined'
- * typeOf(false) => 'boolean'
- * typeOf(Symbol()) => 'symbol'
- * typeOf(Symbol()) => 'symbol'
- * typeOf(function(){}) =. 'function'
+ * @param obj mixed
+ * @return String
+ * 	typeOf({}) => 'object'
+ * 	typeOf([]) => 'array'
+ * 	typeOf('') => 'string'
+ * 	typeOf(.1) => 'number'
+ * 	typeOf(/x/) => 'regexp'
+ * 	typeOf(null) => 'null'
+ * 	typeOf() => 'undefined'
+ * 	typeOf(undefined) => 'undefined'
+ * 	typeOf(false) => 'boolean'
+ * 	typeOf(Symbol()) => 'symbol'
+ * 	typeOf(Symbol()) => 'symbol'
+ * 	typeOf(function(){}) =. 'function'
  */
 function typeOf(obj) {
 	return Object.prototype.toString.call(obj).match(/^\[.* (.*)\]$/)[1].toLowerCase();
+}
+
+/**
+ * 返回当前时间戳
+ * return int
+ */
+function getTimestamp() {
+	return +new Date();
 }
