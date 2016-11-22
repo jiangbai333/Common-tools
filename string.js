63971fbcd9f26a31cdf06243de5f081b01557432
jiangbai333@163.com <jiangbai333@163.com>
2016-11-22 22:56:12 +0800

    优化匹配算法 增加少量注释

--- string.js
+++ string.js
@@ -5,35 +5,44 @@
  */
 `use strict`
 
+/**
+ *      返回被格式化的日期或时间戳
+ * @param param int|String "start"|"end"|"Nmonth"|"Nyear"(N为任意整数)
+ * @return String|int
+ * @example
+ *  "Y年M月D日 H:F:S-ms".format("start"); //2016年11月22日 00:00:00-0
+ *  "Y-M-D H:F:S-ms".format("end"); //2016-11-22 23:59:59-999
+ */
 String.prototype.format = function(param) {
     var type = typeof param,
+        reg = "^((start)|(end)|(-?\\d*month)|(-?\\d*year))$",
         date;
     
     if ( "number" === type ) {
         date = new Date(+new Date + param * 60 * 60 * 1000)
+    } else if (new RegExp(reg, "i").test(param)) {
+        var temp = new Date();
+        if ( RegExp.$2 ) {
+            temp.setHours(0), temp.setMinutes(0), temp.setSeconds(0), temp.setMilliseconds(0);
+        } else if ( RegExp.$3 ) {
+            temp.setHours(23), temp.setMinutes(59), temp.setSeconds(59), temp.setMilliseconds(999);
+        } else if ( RegExp.$4 ) {
+            var num = parseInt(RegExp.$4);
+            temp.setMonth(temp.getMonth() + num);
+        } else if ( RegExp.$5 ) {
+            var num = parseInt(RegExp.$5);
+            temp.setFullYear(temp.getFullYear() + num);
+        }
+        
+        date = temp;
     } else {
-        date = new Date(parseInt(param.replace(/^((start)|(end)|(-?\d*month)|(-?\d*year))$/i, function($0, $1, $2, $3, $4, $5) {
-            var temp = new Date();
-            if ( $2 ) {
-                temp.setHours(0), temp.setMinutes(0), temp.setSeconds(0), temp.setMilliseconds(0);
-            } else if ( $3 ) {
-                temp.setHours(23), temp.setMinutes(59), temp.setSeconds(59), temp.setMilliseconds(999);
-            } else if ( $4 ) {
-                var num = parseInt($4);
-                temp.setMonth(temp.getMonth() + num);
-            } else if ( $5 ) {
-                var num = parseInt($5);
-                temp.setFullYear(temp.getFullYear() + num);
-            }
-            
-            return +temp;
-        })));
+        return this.valueOf();
     }
     
     if ( this === "timestamp" ) {
         return +date;
     } else {
-        var temp = {
+        var dec = {
             "Y" : date.getFullYear(),
             "y" : date.getFullYear() - 2000,
             "M" : (function(M) {return M < 10 ? "0" + M : M;})(date.getMonth() + 1),
@@ -51,22 +60,20 @@ String.prototype.format = function(param) {
         
         var str = this.valueOf();
         
-        for ( var o in temp ) {
+        for ( var o in dec ) {
             if (new RegExp("(" + o + ")").test(str)) {
-                str = str.replace(RegExp.$1, temp[o])
+                str = str.replace(RegExp.$1, dec[o])
             }
         }
-        
         date = str;
     }
-    
     return date;
 }
 
 String.prototype.engine = function($) {
     
     return (function(context, $, callback) {
-        
+        /** 判断参数类型 */
         var tempType = Object.prototype.toString.call($).match(/^\[.* (.*)\]$/)[1].toLowerCase(),
         
             /** 模板变量键值类型：true{String},false{Object|Array...} */
@@ -78,9 +85,11 @@ String.prototype.engine = function($) {
                 }
                 return true;
             })($),
-            reg = /(\\)?\{\$([^\{\}\\]+)?\}/g,
+            /** 匹配模板变量 */
+            reg = /(\\)?\{\$([^\{\}\\]+)?\}/g, 
             rt = new Array();
-            
+        
+        /** 如果参数位 Array 并且键值为 Array 或 Object 则循环替换模板变量 */
         if ( "array" === tempType && !valueType) {
             for (var o in $) {
                 rt.push(callback(context, $[o], reg));
